387e16114a4262b0a4e64dd5b5110b98b550739f
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2022-11-23 23:28:09 -0300

    ✨ feat: Entities created

--- src/app/movies/entities/movie.entity.ts
+++ src/app/movies/entities/movie.entity.ts
@@ -1,1 +1,19 @@
-export class Movie {}
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  director: string;
+
+  @Column()
+  releaseDate: number;
+
+  @Column()
+  genre: string;
+}
--- src/app/users/entities/user.entity.ts
+++ src/app/users/entities/user.entity.ts
@@ -1,1 +1,38 @@
-export class User {}
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { hashSync } from "bcrypt";
+
+@Entity({ name: "users" })
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ name: "first_name" })
+  firstName: string;
+
+  @Column({ name: "last_name" })
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: string;
+
+  @BeforeInsert()
+  hashPassword() {
+    this.password = hashSync(this.password, 10);
+  }
+}
