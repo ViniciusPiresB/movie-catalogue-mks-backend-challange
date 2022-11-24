42f07a03adb4b58d7161acb94bfaa946c64c934e
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2022-11-23 23:20:54 -0300

    ✨ feat: DTOs created

--- src/app/movies/dto/create-movie.dto.ts
+++ src/app/movies/dto/create-movie.dto.ts
@@ -1,1 +1,31 @@
-export class CreateMovieDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateMovieDto {
+  @ApiProperty({
+    title: "Name of movie",
+    type: String,
+    description: "This is a required property"
+  })
+  name: string;
+
+  @ApiProperty({
+    title: "Director of movie",
+    type: String,
+    description: "This is a required property"
+  })
+  director: string;
+
+  @ApiProperty({
+    title: "Release Date",
+    type: Number,
+    description: "This is a required property"
+  })
+  releaseDate: number;
+
+  @ApiProperty({
+    title: "Genre of movie",
+    type: String,
+    description: "This is a required property"
+  })
+  genre: string;
+}
--- src/app/users/dto/create-user.dto.ts
+++ src/app/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsEmail, Matches } from "class-validator";
+import { messagesHelper } from "../../helper/message.helper";
+import { regExHelper } from "../../helper/regex.helper";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(regExHelper.password, { message: messagesHelper.PASSWORD_VALID })
+  password: string;
+}
